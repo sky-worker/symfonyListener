adfd1f496d1c96bf0ef9f38d04287ec4f90cba46
sky-worker <anthony.ngati@live.fr>
2019-05-06 14:34:04 +0200

    Try the subscriber stuff

--- src/EventListener/BienvenueListener.php
+++ src/EventListener/BienvenueListener.php
@@ -5,12 +5,25 @@ namespace App\EventListener;
 
 use App\Events\BienvenueUtilisateur;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class BienvenueListener
+class BienvenueListener implements EventSubscriberInterface
 {
 
     public function updateResponse(BienvenueUtilisateur $event)
     {
         die('Yeah !');
     }
+
+    public static function getSubscribedEvents()
+    {
+        // Liste des évènements, méthodes et priorités
+        return [
+            'bienvenue.utilisateur' => [
+                ['updateResponse', -10]
+            ],
+        ];
+    }
+
+
 }
